4b850a81b2446a18336c9a4a250181ec5ab1d1af
Renato Jiquiriçá Ramos <rjramos70@RenatosMBP92247.lan>
2024-04-06 16:44:42 +0100

    First commit

---
+++ src/main/kotlin/com/bestbet/BestBetApplication.kt
@@ -0,0 +1,11 @@
+package com.bestbet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BestBetApplication
+
+fun main(args: Array<String>) {
+    runApplication<BestBetApplication>(*args)
+}
---
+++ src/test/kotlin/com/bstbet/BstBetApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bestbet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BestBetApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
